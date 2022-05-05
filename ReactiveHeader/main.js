07da628e8dff453fe0838545edacb7ce97ab2f2d
SangHyun <93238342+qkrsh@users.noreply.github.com>
2022-05-05 15:11:01 +0900

    Delete ReactiveHeader directory

--- ReactiveHeader/main.js
+++
@@ -1,8 +0,0 @@
-const toggleBtn = document.querySelector('.navbar__toogleBtn');
-const menu1 = document.querySelector('#menu');
-const icons = document.querySelector('#link');
-
-toggleBtn.addEventListener('click', () =>{
-    menu1.classList.toggle('active');
-    icons.classList.toggle('active');
-});
